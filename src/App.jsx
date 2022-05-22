6bb20c3c792c67158c6bd73d0a33c452006d0071
Masrur Alam <masrur.jsr@gmail.com>
2022-05-22 07:14:58 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Offers from './components/Offers';
+import Filters from './components/Filters';
+import Restaurants from './components/Restaurants';
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+      <Offers/>
+      <Filters/>
+      <Restaurants/>
+    </div>
+  );
+}
+
+export default App;
